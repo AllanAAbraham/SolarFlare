b485222389cae891b64741f091f42b1d12b3beb7
AllanAAbraham <allan.aju.abraham@gmail.com>
2022-08-13 18:56:03 -0400

    Setting up Django and Files

---
+++ SolarFlare/SolarAPI/Models/SolarPower.py
@@ -0,0 +1,33 @@
+class SolarPower:
+    
+    def __init__(self, name: str, capacity: float, address: str, city: str, state: str, zip: str) -> None:
+        self.__name = name
+        self.__capacity = capacity
+        self.__address = address
+        self.__city = city
+        self.__state = state
+        self.__zip = zip
+
+    @property
+    def Name(self):
+        return self.__name
+    
+    @property
+    def Capacity(self):
+        return self.__capacity
+
+    @property
+    def Address(self):
+        return self.__address
+
+    @property
+    def City(self):
+        return self.__city
+
+    @property
+    def State(self):
+        return self.__state
+
+    @property
+    def Zip(self):
+        return self.__zip
